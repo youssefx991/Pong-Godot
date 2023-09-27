b7c54bfcc833ab44603d612f316a0d6a28b01f8f
youssef wagih <youssefwagih212@gmail.com>
2023-09-28 00:42:43 +0300

    Game reads from file at first

--- Singleton/Singleton.gd
+++ Singleton/Singleton.gd
@@ -8,9 +8,7 @@ var is_hard
 var player_name
 var player_score
 
-var players = [
-	{"name": "Admin", "score": 0}
-]
+var players = []
 
 
 
@@ -18,7 +16,8 @@ func sort_by_score(a, b):
 	return a["score"] > b["score"]
 	
 func _ready():
-	
+	file_read()
+	players.sort_custom(self, 'sort_by_score')
 	player_name = ""
 	player_score = 0
 	value = 150
@@ -53,3 +52,42 @@ func player_exists(name):
 		if (name == player["name"]):
 			return true
 	return false
+
+func file_read():
+	
+
+# Open the file for reading
+	var file = File.new()
+	if file.open("res://players.txt", File.READ) == OK:
+		print("file opened")
+		while !file.eof_reached():
+			print("didn't reach end of file")
+			var line = file.get_line().strip_edges()  # Read and clean the line
+			if line.empty():
+				continue  # Skip empty lines
+
+			# Split the line into key and value
+			var parts = line.split(":")
+			if parts.size() == 2:
+				print("split with , ")
+				var key = parts[0].strip_edges()
+				var value = parts[1].strip_edges()
+
+				# Add the key-value pair to the current player dictionary
+				var current_player = {
+					"name": key,
+					"score": int(value)
+				}
+				
+
+				# Add the current player to the 'players' array
+				players.append(current_player)
+		print("Wrote success")
+		file.close()
+
+	# Print the 'players' dictionary (optional)
+#	for player in players:
+#		print("Name:", player["name"])
+#		#print("Score:", player["score"])
+#		print()
+			
